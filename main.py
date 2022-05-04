be187359a7ad6f93438ccc0ed932a5f0bac54253
snake <admin@viasnake.com>
2022-05-05 02:28:07 +0900

    Add var type

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def apply_rules(iptables, target_rules):
+def apply_rules(iptables: list, target_rules: list) -> list:
     for r in target_rules:
         category = r[0]
         protocol = r[1]
@@ -10,12 +10,14 @@ def apply_rules(iptables, target_rules):
     return iptables
 
 
-def find_rule(iptables, target_rule):
+def find_rule(iptables: list, target_rule: str) -> int:
     index = iptables.index(target_rule)
     return index
 
 
-def add_rule(iptables, index, protocol, port):
+def add_rule(iptables: list, index: int, protocol: str, port: int) -> list:
+    port = str(port)
+    rule = ""
     if protocol == "TCP":
         rule = "-A INPUT -p tcp -m state --state NEW -m tcp --dport " + port + " -j ACCEPT"
     elif protocol == "UDP":
@@ -30,7 +32,7 @@ def load_iptables(path):
     return iptables
 
 
-def write_iptables(path, iptables):
+def write_iptables(path: str, iptables: list):
     with open(path, 'w') as f:
         f.writelines("%s\n" % t for t in iptables)
 
@@ -38,11 +40,11 @@ def write_iptables(path, iptables):
 def main():
     path = "/etc/iptables/rules.v4"
     target_rules = [
-        ["ADD", "TCP", "2377", "-A INPUT -p tcp -m state --state NEW -m tcp --dport 22 -j ACCEPT"],
-        ["ADD", "TCP", "7946", "-A INPUT -p tcp -m state --state NEW -m tcp --dport 2377 -j ACCEPT"],
-        ["ADD", "UDP", "7946", "-A INPUT -p tcp -m state --state NEW -m tcp --dport 7946 -j ACCEPT"],
-        ["ADD", "TCP", "4789", "-A INPUT -p udp -m state --state NEW -m udp --dport 7946 -j ACCEPT"],
-        ["ADD", "UDP", "4789", "-A INPUT -p tcp -m state --state NEW -m tcp --dport 4789 -j ACCEPT"]
+        ["ADD", "TCP", 2377, "-A INPUT -p tcp -m state --state NEW -m tcp --dport 22 -j ACCEPT"],
+        ["ADD", "TCP", 7946, "-A INPUT -p tcp -m state --state NEW -m tcp --dport 2377 -j ACCEPT"],
+        ["ADD", "UDP", 7946, "-A INPUT -p tcp -m state --state NEW -m tcp --dport 7946 -j ACCEPT"],
+        ["ADD", "TCP", 4789, "-A INPUT -p udp -m state --state NEW -m udp --dport 7946 -j ACCEPT"],
+        ["ADD", "UDP", 4789, "-A INPUT -p tcp -m state --state NEW -m tcp --dport 4789 -j ACCEPT"]
     ]
     iptables = load_iptables(path)
     iptables = apply_rules(iptables, target_rules)
